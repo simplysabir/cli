9773ad94547f7523a4533f2d2d785c505ac8fdd5
simplysabir <simplysabir@gmail.com>
2023-08-28 09:47:02 +0530

    v1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cli
+it contains all of the info related to me. just run the command and it will be delivered to your terminal too.
+
+just run ` npm i -g simplysabir `  
+
+after that run this command ` simplysabir `
+
+it will display all of my info.
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+//* Declare the program
+const program = new Command();
+
+//* Add actions onto that CLI
+
+program
+    .name('simplysabir')
+    .description('portfolio of sabir khan.')
+    .version('1.1.0')
+    
+    .action(() => {
+        console.log(`\nintro about me:
+        \n20-year-old CS undergrad from mumbai. I'm a full stack developer, web 3 developer, and a programmer. I'm also a member of Superteam.
+        \ncurrently studying in my final year of CS B. Tech, I'm based out of mumbai, where I am honing my skills and deepening my understanding of the latest trends and technologies in the field of computer science.\n
+        `);
+        
+        console.log(`\nmy experience:
+        \nde_plan proto : June 2023 - Present
+        \nWorked as a Full Stack Developer to\n
+- Developed the API's
+- Worked on Frontend App \n
+Building the future of location-based applications through proto. Proto is a spatial data registry with decentralized proof-of-location and location-based services on the Solana blockchain
+        `);
+
+        console.log(`
+        \nsplitwave : Feb 2023 - May 2023
+        \nFounding Engineer and Blockchain Developer.
+        \n I Have helped in : \n
+- Getting Started from the Scartch.
+- Helped in Building the Extension & xNFT for the dApp.
+- Worked as a Founding Engineer. \n        `);
+
+        console.log(`\nhere's all links related to me:
+        \nportfolio : https://simplysabir.vercel.app/
+        \nportfolio : https://github.com/simplysabir
+        \nsocial links : https://campsite.bio/simplysabir\n
+        `);
+    });
+
+program.parse(process.argv);
